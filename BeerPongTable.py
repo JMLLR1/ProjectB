a3bc79c3a2b3ddba3275d84cf3f34a79901dbfe7
Jens Müller <jens.mueller98@gmx.de>
2022-01-22 22:38:25 +0100

    initial commit

---
+++ BeerPongTable.py
@@ -0,0 +1,20 @@
+class BeerPong:
+
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+    def start_led_strip(self):
+        pass
+
+    def open_animation_file(self):
+        pass
+
+    def write_to_matrix(self):
+        pass
+
+    def change_led_arrangement(self):
+        pass
+
